2a84e5339a1a7b1856ad03b27a7fa1891fec0695
Naveen CS <catchspider2002@gmail.com>
2023-03-21 12:20:18 +0530

    Updated icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script>
   import '../app.css'
   import '../prism.css'
-  import MoonIcon from 'heroicons-svelte/solid/MoonIcon.svelte'
-  import SunIcon from 'heroicons-svelte/solid/SunIcon.svelte'
   import { browser } from '$app/environment'
   import { name, logo } from '$lib/info'
   import { page } from '$app/stores'
+
+  let nightMode = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-current fill-none stroke-2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+                      <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
+                   </svg>`
+
+  let dayMode = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-current fill-none stroke-2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
+                    <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+                 </svg>`
 
   let isDarkMode = browser ? Boolean(document.documentElement.classList.contains('dark')) : true
 
@@ -47,8 +54,11 @@
           }
         }}
       >
-        <MoonIcon class="hidden text-zinc-500 dark:block" />
-        <SunIcon class="block text-zinc-400 dark:hidden" />
+        <!-- <MoonIcon class="hidden text-zinc-500 dark:block" />
+        <SunIcon class="block text-zinc-400 dark:hidden" /> -->
+        <span class="hidden dark:block">{@html nightMode}</span>
+        <span class="block dark:hidden">{@html dayMode}</span>
+
       </button>
     </header>
     <main
